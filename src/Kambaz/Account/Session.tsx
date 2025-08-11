1ca51c82c171af80de876c8e6a439b78a82eb019
wanke20 <122842952+wanke20@users.noreply.github.com>
2025-08-11 15:33:17 -0400

    fix profile fetching

--- src/Kambaz/Account/Session.tsx
+++ src/Kambaz/Account/Session.tsx
@@ -7,8 +7,10 @@ export default function Session({ children }: { children: any }) {
   const dispatch = useDispatch();
   const fetchProfile = async () => {
     try {
-      const currentUser = await client.profile();
-      dispatch(setCurrentUser(currentUser));
+      if (!pending) {
+        const currentUser = await client.profile();
+        dispatch(setCurrentUser(currentUser));
+      }
     } catch (err: any) {
       console.error(err);
     }
